9bf955069d264060e0efa96bea2af0b6d5c7c93c
tobinski <tobinski@dodis.ch>
2018-03-04 18:02:53 +0100

    Update some basic tests for the OpenSeadragon directive

--- src/app/image-viewer/osd-viewport.directive.spec.ts
+++ src/app/image-viewer/osd-viewport.directive.spec.ts
@@ -1,8 +1,30 @@
 import { OsdViewportDirective } from './osd-viewport.directive';
+import {TestBed, ComponentFixture} from '@angular/core/testing';
+import {Component, ViewContainerRef} from '@angular/core';
+
+@Component({
+    template: `<div #osdViewport></div>`
+})
+class TestOsdComponent {
+}
+
 
 describe('OsdViewportDirective', () => {
+
+    let component: TestOsdComponent;
+    let fixture: ComponentFixture<TestOsdComponent>;
+    let viewContainerRef: ViewContainerRef;
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [TestOsdComponent, OsdViewportDirective]
+        });
+        fixture = TestBed.createComponent(TestOsdComponent);
+        component = fixture.componentInstance;
+        viewContainerRef = fixture.elementRef.nativeElement.viewContainer;
+    });
+
   it('should create an instance', () => {
-    const directive = new OsdViewportDirective();
+    const directive = new OsdViewportDirective(viewContainerRef);
     expect(directive).toBeTruthy();
   });
 });
