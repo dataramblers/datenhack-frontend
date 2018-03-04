0bb3a70a18a494f1305f2d5da79dfcbe505597d1
tobinski <tobinski@dodis.ch>
2018-03-04 18:03:53 +0100

    Update tests for the ImageOverview component

--- src/app/image-overview/image-overview.component.spec.ts
+++ src/app/image-overview/image-overview.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ImageOverviewComponent } from './image-overview.component';
+import {MaterialModule} from '../material.module';
+import {By} from '@angular/platform-browser';
 
 describe('ImageOverviewComponent', () => {
   let component: ImageOverviewComponent;
@@ -8,7 +10,8 @@ describe('ImageOverviewComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ImageOverviewComponent ]
+      declarations: [ ImageOverviewComponent ],
+        imports: [MaterialModule]
     })
     .compileComponents();
   }));
@@ -16,10 +19,18 @@ describe('ImageOverviewComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ImageOverviewComponent);
     component = fixture.componentInstance;
+    component.imageNames = Array(10).fill('test');
+    component.columns = 2;
     fixture.detectChanges();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+      expect(component).toBeTruthy();
+  });
+
+  it('should create 10 preview images', () => {
+      fixture.detectChanges();
+      expect(fixture.debugElement.queryAll(By.css('mat-grid-tile')).length === 10)
+          .toBe(true);
   });
 });
