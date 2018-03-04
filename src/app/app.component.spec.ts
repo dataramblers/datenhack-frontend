76aab4137cdbe8aa48579afedcb2ad45c92bc36f
tobinski <tobinski@dodis.ch>
2018-03-04 18:04:17 +0100

    Update tests for the App component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,19 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {ImageOverviewModule} from './image-overview/image-overview.module';
+import {ImageViewerModule} from './image-viewer/image-viewer.module';
+import {MaterialModule} from './material.module';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
       ],
+      imports: [
+          ImageOverviewModule,
+          ImageViewerModule,
+          MaterialModule
+      ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
