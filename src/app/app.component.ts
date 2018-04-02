ab82e21f163964e7f22d7aa3998d060b90022510
tobinski <tobinski@dodis.ch>
2018-04-02 21:23:12 +0200

    Wire the components together

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { environment } from '../environments/environment';
+import {ImagesService} from './images.service';
+import {Image} from './shared/image';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
-  imageName = environment.api + '01/02/0001.jp2';
-  imageNames = Array(10).fill(this.imageName);
+  imageName = '//' + environment.iiif + '/Sozarch_F_5025-Fa-013.tif';
+  imageNames: Image[];
+
+  constructor(private imagesService: ImagesService){  }
+
+    /**
+     * Init
+     */
+  ngOnInit() {
+    this.imagesService.getImages().subscribe((images) => {
+      this.imageNames = images;
+    });
+  }
+
+  setImage($event) {
+    this.imageName = '//' + environment.iiif + '/' + $event + '.tif';
+  }
 }
