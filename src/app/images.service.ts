de6c2faf07a65f14e46b17137d1065b61a0f3eba
Sebastian Schüpbach <sebastian.schuepbach@unibas.ch>
2018-05-16 17:17:53 +0200

    Change method getImages to getImagesBySearchTerm with respective parameter
    Signed-off-by: Sebastian Schüpbach <sebastian.schuepbach@unibas.ch>

--- src/app/images.service.ts
+++ src/app/images.service.ts
@@ -1,32 +1,26 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {UrlHelper} from './shared/url-helper';
-import {Image} from './shared/image';
+import { HttpClient } from '@angular/common/http';
+import { UrlHelper } from './shared/url-helper';
+import { Image } from './shared/image';
 import 'rxjs/add/operator/map';
-import {ElasticSearchResponse} from './shared/elastic-search-response';
-import {forEachComment} from 'tslint';
+import { ElasticSearchResponse } from './shared/elastic-search-response';
 
 @Injectable()
 export class ImagesService {
 
-  constructor(private http: HttpClient) { }
-
-    /**
-     * Get all images from ES
-     * todo: the query is just for debug
-     * @returns {Observable<Image[]>}
-     */
-  getImages() {
-    return this.http
-        .get<ElasticSearchResponse>(UrlHelper.getEsUrl('_search', ['q=fa']))
-        .map((response) => {
-          return response.hits.hits;
-        }).map((hits) => {
-          const res = [];
-          for ( let i = 0; i < hits.length; i++ ) {
-              res.push(Image.imageFabric(hits[i]._source));
-          }
-          return res;
-      });
+  constructor(private http: HttpClient) {
   }
+
+  /**
+   * Get all images from ES
+   * todo: the query is just for debug
+   * @returns {Observable<Image[]>}
+   */
+  getImagesBySearchTerm(searchTerm?: string = '*:*', offset?: number, limit?: number = 10) {
+    return this.http
+      .get<ElasticSearchResponse>(UrlHelper.getEsUrl('_search', [ `q=${searchTerm}`, `from=${offset}`, `size=${limit}` ]))
+      .map((response) => response.hits.hits)
+      .map((hits) => hits.map(x => Image.imageFabric(x._source)));
+  }
+
 }
